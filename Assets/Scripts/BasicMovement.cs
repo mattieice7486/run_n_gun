ef0601522623146784a5ef8f8379f1f20bc6c0c2
dave <supplebiscuit@gmail.com>
2019-02-17 15:20:22 -0500

    0.0002

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -7,9 +7,13 @@ public class BasicMovement : MonoBehaviour
     public float speed;
     private Rigidbody2D myRigidbody;
     private Vector3 change;
+    private Animator animator;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
     }
 
@@ -19,12 +23,23 @@ public class BasicMovement : MonoBehaviour
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
-        if(change != Vector3.zero)
-        {
-            MoveCharacter();
-        }
+        UpdateAnimationAndMove();
     }
 
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            Debug.Log(change.x);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
     void MoveCharacter()
     {
         myRigidbody.MovePosition(transform.position + change * speed * Time.deltaTime);
